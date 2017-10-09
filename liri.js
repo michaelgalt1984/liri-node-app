17e5ed854a940dac7e6fe9c8650f39f5cecd4018
michaelgalt1984 <michael.galt.1984@gmail.com>
2017-10-09 18:28:07 -0500

    Set up the basic requests for packages from NPM on my liri.js so that I would have the appropriate node modules.  Now it is just coding time, once I have all the accounts and keys.

---
+++ liri.js
@@ -0,0 +1,11 @@
+// Stuff necessary to make the node operations work
+var fs = require("fs");
+
+var request = require("request");
+
+var keys = require("./keys.js");
+
+var Twitter = require("twitter");
+
+var Spotify = require("node-spotify-api");
+
