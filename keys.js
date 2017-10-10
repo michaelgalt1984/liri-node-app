ee3cb7ecc1499e9236f5fe98a9bc1d7b72a2269a
michaelgalt1984 <michael.galt.1984@gmail.com>
2017-10-10 06:34:40 -0500

    Got my Twitter keys and updated that accordingly.  Now to do the same for Spotify.

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'hskwGW2o63QJl9zKfp4a1XQvG',
+  consumer_secret: 'a8hFw5M6fONx2vpnrZ60uZSh6bEP1kuG309V25JQZb3tDGd7NV',
+  access_token_key: '25194211-QOMbJAgQRFtjFI8YUw0MBtStCA5jFZiTDrmkGzl0r',
+  access_token_secret: 'OsrxVyVrvjsul3Qshfat4U3qhooWOdMAsUpZnHksrwVtz',
 }
 
 module.exports = twitterKeys;
