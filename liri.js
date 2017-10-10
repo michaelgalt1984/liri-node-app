cadfd1473181e583beb1eaa46b2247fc0db2bf92
michaelgalt1984 <michael.galt.1984@gmail.com>
2017-10-10 06:52:21 -0500

    Went and set up the basic requirements for the liri JS.  Time to start coding...

--- liri.js
+++ liri.js
@@ -1,11 +1,34 @@
 // Stuff necessary to make the node operations work
+
+var keys = require("./keys.js");
+
 var fs = require("fs");
 
 var request = require("request");
 
-var keys = require("./keys.js");
 
 var Twitter = require("twitter");
 
 var Spotify = require("node-spotify-api");
+
+
+//================================================================================================================================================================================================================================================//
+
+
+// Mandatory Commands for liri
+
+// This will show your last 20 tweets and when they were created at in your terminal/bash window.
+my-tweets
+
+// show the following information about the song in your terminal/bash window: artist(s), song's name, preview link of the song from Spotify, album that the song is from. If no song is provided then your program will default to "The Sign" by Ace of Base.
+spotify-this-song '<song name here>'
+
+// This will output the following information to your terminal/bash window: Title of the movie, Year the movie came out, IMDB Rating of the movie, Rotten Tomatoes Rating of the movie, Country where the movie was produced, Language of the movie, Plot of the movie, Actors in the movie. If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.' 40e9cece
+movie-this '<movie name here>'
+
+// This will take the text inside of random.txt and then use it to call one of LIRI's commands. It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+do-what-it-says
+
+
+//================================================================================================================================================================================================================================================//
 
