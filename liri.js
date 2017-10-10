8abdd36e4e26f317ffef165772cb03ad1354df79
michaelgalt1984 <michael.galt.1984@gmail.com>
2017-10-10 07:13:49 -0500

    Put in the required commands for Twitter, according to the documentation that they provided... but, haven't figured out how to get that associated with the node command to actually do that thing. I guess that I need to actually call one of the variables they made me create?

--- liri.js
+++ liri.js
@@ -2,14 +2,13 @@
 
 var keys = require("./keys.js");
 
-var fs = require("fs");
-
 var request = require("request");
-
 
 var Twitter = require("twitter");
 
 var Spotify = require("node-spotify-api");
+
+var fs = require("fs");
 
 
 //================================================================================================================================================================================================================================================//
@@ -18,17 +17,27 @@ var Spotify = require("node-spotify-api");
 // Mandatory Commands for liri
 
 // This will show your last 20 tweets and when they were created at in your terminal/bash window.
-my-tweets
+// my-tweets
 
 // show the following information about the song in your terminal/bash window: artist(s), song's name, preview link of the song from Spotify, album that the song is from. If no song is provided then your program will default to "The Sign" by Ace of Base.
-spotify-this-song '<song name here>'
+// spotify-this-song '<song name here>'
 
 // This will output the following information to your terminal/bash window: Title of the movie, Year the movie came out, IMDB Rating of the movie, Rotten Tomatoes Rating of the movie, Country where the movie was produced, Language of the movie, Plot of the movie, Actors in the movie. If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.' 40e9cece
-movie-this '<movie name here>'
+// movie-this '<movie name here>'
 
 // This will take the text inside of random.txt and then use it to call one of LIRI's commands. It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-do-what-it-says
+// do-what-it-says
 
 
 //================================================================================================================================================================================================================================================//
 
+// Twitter
+
+var client = new Twitter(keys.twitterkeys);
+
+var params = {screen_name: 'michael_galt'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
