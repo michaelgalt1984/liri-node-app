e1e4d1316a3948337ad5a1a3806acaf9f6b55300
michaelgalt1984 <michael.galt.1984@gmail.com>
2017-10-10 09:01:00 -0500

    Got something for OMBD in... but it doesn't seem to be working, and the documentation is pretty terrible for how to use the OMBD node module.  As far as I can tell, the way that I have it written is consistent with their example, it just doesn't seem to actually work.

--- liri.js
+++ liri.js
@@ -62,3 +62,25 @@ spotify.search({
 	console.log(data); 
 });
 
+//================================================================================================================================================================================================================================================//
+
+// OMDB
+
+var HOST = 'http://www.omdbapi.com/';
+
+omdb.search(process.argv[2], function(err, movies) {
+    if(err) {
+        return console.error(err);
+    }
+ 
+    if(movies.length < 1) {
+        return console.log('No movies were found!');
+    }
+ 
+    movies.forEach(function(movie) {
+        console.log('%s (%d) %d/10', movie.title, movie.year, movie.imdb.rating);
+    	console.log(movie.plot);
+    });
+});
+
+
